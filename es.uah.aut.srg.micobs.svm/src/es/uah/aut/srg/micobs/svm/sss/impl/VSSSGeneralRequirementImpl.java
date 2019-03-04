8b2c3a02b443b81fc40fc855836b4da5480c30a2
Aaron Montalvo <aaron.montalvo.taravillo@gmail.com>
2019-03-04 18:25:59 +0100

    Added skeleton of getItems method to VSSSGeneralRequirementImpl class

--- es.uah.aut.srg.micobs.svm/src/es/uah/aut/srg/micobs/svm/sss/impl/VSSSGeneralRequirementImpl.java
+++ es.uah.aut.srg.micobs.svm/src/es/uah/aut/srg/micobs/svm/sss/impl/VSSSGeneralRequirementImpl.java
@@ -13,9 +13,13 @@ package es.uah.aut.srg.micobs.svm.sss.impl;
 import es.uah.aut.srg.micobs.svm.sss.VSSSGeneralRequirement;
 import es.uah.aut.srg.micobs.svm.sss.sssPackage;
 import es.uah.aut.srg.micobs.svm.tdm.VTraceableDocumentAbstractSection;
+import es.uah.aut.srg.micobs.svm.tdm.VTraceableDocumentItem;
+import es.uah.aut.srg.micobs.svm.tdm.tdmPackage;
 import es.uah.aut.srg.micobs.svm.tdm.impl.VTraceableDocumentFixedGroupImpl;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
 
 /**
  * <!-- begin-user-doc -->
@@ -42,6 +46,14 @@ public class VSSSGeneralRequirementImpl extends VTraceableDocumentFixedGroupImpl
 	@Override
 	protected EClass eStaticClass() {
 		return sssPackage.Literals.VSSS_GENERAL_REQUIREMENT;
+	}
+	
+	@Override
+	public EList<VTraceableDocumentItem> getItems() {
+		EList<VTraceableDocumentItem> items = new EObjectResolvingEList<VTraceableDocumentItem>(VTraceableDocumentItem.class,
+				this, tdmPackage.VTRACEABLE_DOCUMENT_ABSTRACT_GROUP__ITEMS);
+		items.add();
+		return items;
 	}
 	
 	@Override
